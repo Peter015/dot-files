b35fb2f97249d5990e0c449464561a848aab5f36
Peter Zimmermann <peterz@localhost.localdomain>
2022-08-27 01:24:48 -0400

    first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+# Set the manpage
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+#set aliases
+alias ls='exa --color=always --group-directories-first'
+alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+alias grep='rg'
+alias cat='bat'
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/peterz/.local/share/omf/themes/pie/fish_prompt.fish
