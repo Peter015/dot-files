fe45f884e67384d69983d13e340e2d3d5a381908
Peter Zimmermann <pzimm30@gmail.com>
2022-09-16 12:08:14 -0400

    added to emacs config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -22,6 +22,32 @@
 (use-package tree-sitter)
 (use-package tree-sitter-langs)
 
+;; lsp
+(use-package eglot)
+
+;; merlin for ocaml
+(use-package merlin)
+
+;; ivy
+(use-package ivy
+    :diminish
+    :bind (("C-s" . swiper)
+        :map ivy-minibuffer-map
+        ("TAB" . ivy-alt-done)
+        ("C-l" . ivy-alt-done)
+        ("C-j" . ivy-next-line)
+        ("C-k" . ivy-previous-line)
+        :map ivy-switch-buffer-map
+        ("C-k . ivy-previous-line")
+        ("C-l" . ivy-done)
+        ("C-d" . ivy-switch-buffer-kill)
+        :map ivy-reverse-i-search-map
+        ("C-k" . ivy-previous-line)
+        ("C-d" . ivy-reverse-i-search-kill))
+    :config
+    (ivy-mode 1))
+        
+
 ;; settings
 (setq inhibit-startup-message t
     visible-bell t
@@ -36,17 +62,24 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
-
 ;; set the theme
 (load-theme 'wombat t)
 
 
-;; ivy configuration
+;; eglot autoconnet
+(add-hook 'foo-mode-hook 'eglot-ensure)
 
 
 ;; treesitter
 (global-tree-sitter-mode)
 (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+
+;; merlin support
+(push "/home/peterz/.config/emacs/elpa" load-path)
+(autoload 'merlin-mode "merlin" "Merlin mode" t)
+(add-hook 'tuareg-mode-hook #'merlin-mode)
+(add-hook 'caml-mode-hook #'merlin-mode)
+
 
 ;; sets up slime, common lisp IDE based on emacs
 (load (expand-file-name "~/.quicklisp/slime-helper.el"))
