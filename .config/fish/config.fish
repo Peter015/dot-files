5a514b07e4985e1e887204c1b2add6d9651a21c6
Peter Zimmermann <pzimm30@gmail.com>
2022-09-30 12:25:31 -0400

    stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,1 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+/nix/store/lya4x1cpsnflwag1bzdj0ay51j2951rc-home-manager-files/.config/fish/config.fish
--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -3,8 +3,8 @@
 {
     # Home Manager needs a bit of information about you and the
     # paths it should manage.
-    home.username = "jdoe";
-    home.homeDirectory = "/home/jdoe";
+    home.username = "peterz";
+    home.homeDirectory = "/home/peterz";
   
     # This value determines the Home Manager release that your
     # configuration is compatible with. This helps avoid breakage
@@ -15,20 +15,54 @@
     # the Home Manager release notes for a list of state version
     # changes in each release.
     home.stateVersion = "22.05";
+
+    programs.fish = {
+        enable = true;
+        plugins = [
+            {
+                name = "z";
+                src = pkgs.fetchFromGitHub {
+                    owner = "jethrokuan";
+                    repo = "z";
+                    rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
+                    sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
+                };
+            }
+            
+            # { name = "grc"; src = pkgs.fishPlugins.grc; }
+            { name = "pure"; src = pkgs.fishPlugins.pure; }
+        ];
+        
+        interactiveShellInit = "starship init fish | source";
+        
+        shellAliases = {
+            
+        };     
+    };    
+    
     
     
     programs.emacs = {
         enable = true;
-        extraPackages = epkgs: [
-            epkgs.nix-mode
-            epkgs.magit
-            epkgs.tree-sitter
-            epkgs.tree-sitter-langs
-            epkgs.paredit
-            epkgs.tron-legacy-theme
-            epkgs.eglot
-            epkgs.merlin
-        ];
+        # extraPackages = (
+            # epkgs: (with epkgs; [
+                # nix-mode
+                # magit
+                # tree-sitter
+                # tree-sitter-langs
+                # paredit
+                # tron-legacy-theme
+                # eglot
+                # merlin
+                # rust-mode
+            # ])
+        # );
+    };
+    
+    programs.git = {
+        enable = true;
+        userName = "Peter Zimmermann";
+        userEmail = "pzimm30@gmail.com";
     };
   
     # Let Home Manager install and manage itself.
